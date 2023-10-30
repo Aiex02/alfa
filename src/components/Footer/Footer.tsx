4477b48b8c343850ac06ae90112fb71f95b3b25f
Guilherme Aiex <guilhermemedeirosaiex@gmail.com>
2023-10-30 13:26:00 -0300

    fix: space footer itens

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,32 +1,29 @@
-import {
-  FaWhatsapp,
-  FaLinkedin,
-  FaInstagram,
-  FaMapMarkedAlt,
-} from "react-icons/fa";
+import { FaWhatsapp, FaMapMarkerAlt } from "react-icons/fa";
 
 export function Footer() {
   return (
-    <footer id="contact" className="bg-black text-white py-2">
+    <footer id="contact" className="bg-black text-white py-4">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between text-center text-md-left">
-        <div className="mb-2 md:mb-0">
+        <div className="mb-4 md:mb-0">
           <h1 className="text-3xl font-bold text-white">*LOGO*</h1>
-          <p>&copy; 2023 Seu Site. Todos os direitos reservados.</p>
+          <p className="text-xs">&copy; 2023 Seu Site. Todos os direitos reservados.</p>
         </div>
-        <div>
+        <div className="text-sm">
           <div className="flex space-x-4 items-center mb-2">
-            <div className="text-xs">
+            <div>
               <h2 className="font-semibold text-white mb-1">Setor Operacional:</h2>
-              <p>+55 (22) 99878-0228</p>
+              <p className="mb-1">+55 (22) 99878-0228</p>
             </div>
-            <div className="text-xs">
+            <div>
               <h2 className="font-semibold text-white mb-1">Setor Comercial:</h2>
-              <p>+55 (22) 99878-0228</p>
+              <p className="mb-1">+55 (22) 99878-0228</p>
             </div>
           </div>
-          <div className="flex w-64 space-x-4 items-center text-xs ">
-            <FaMapMarkedAlt size={24} />
-            <span>Av.Atlântica, nº 2650, Cavaleiros - Macaé/RJ</span>
+          <div className="text-xs space-x-4 items-center">
+            <span style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+              <FaMapMarkerAlt size={15} />
+              <span>Av.Atlântica, nº 2650, Cavaleiros - Macaé/RJ</span>
+            </span>
           </div>
         </div>
         <div className="flex items-center space-x-4">
